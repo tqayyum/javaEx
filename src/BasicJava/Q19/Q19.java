00cb796fbcac34892109ccc1cbb9a6efc3952bc0
[Tahir Qayyum] <[qayyumtahir00@gmail.com]>
2018-11-12 18:33:07 -0500

    Adding the Q19 package

---
+++ src/BasicJava/Q19/Q19.java
@@ -0,0 +1,16 @@
+package BasicJava.Q19;
+
+/**
+ * 19. Write a Java program to convert a decimal number to binary number. Go to the editor
+ * Input Data:
+ * Input a Decimal Number : 5
+ * Expected Output
+ *
+ * Binary number is: 101
+ */
+
+public class Q19 {
+    public static void main(String [] args) {
+
+    }
+}
